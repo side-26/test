ce5802b406487dd67250406b336f94cde1f2cf13
side-26e <mr72302@gamil.com>
2022-08-15 14:42:12 +0430

    first commit

---
+++ src/components/auth/messageForm/MessageForm.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <form>
+            
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/components/auth/mobileForm/MobileForm.vue
@@ -0,0 +1,37 @@
+<template lang="">
+  <div>
+    <form class="mt-8" @submit.prevent="handleSubmit()">
+      <div class="flex flex-col mt-4 pb-5 border-b-2 border-b-gray-300">
+        <label for="phone_number"> شماره موبایل* </label>
+        <input
+          type="text"
+          placeholder="شماره تلفن خود را وارد کنید"
+          name="phone_number"
+          class="border-0 focus:outline-blue-400 my-4 py-2 rounded-xl placeholder:pr-1 pr-3"
+        />
+      </div>
+      <div class="pt-5 text-white">
+        <button
+          type="submit"
+          class="py-2 px-10 w-full bg-cyan-500 font-medium rounded-xl hover:bg-cyan-600"
+        >
+          ورود به تست
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: "MobileForm",
+  setup(props, { emit }) {
+    const handleSubmit = () => {
+      emit("handleStage");
+    };
+    return {
+      handleSubmit,
+    };
+  },
+};
+</script>
+<style lang=""></style>
